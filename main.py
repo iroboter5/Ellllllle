de37b467825846216eb84b3923c7fa344adc83d7
Devin <100889237+iroboter5@users.noreply.github.com>
2022-12-06 01:23:18 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -203,7 +203,7 @@ def onmessage(update,bot:ObigramClient):
     try:
         thread = bot.this_thread
         username = update.message.sender.username
-        tl_admin_user = os.environ.get('tl_admin_user')
+        tl_admin_user = os.environ.get('nautaii')
 
         #set in debug
         tl_admin_user = 'nautaii'
@@ -453,9 +453,9 @@ def onmessage(update,bot:ObigramClient):
             #    bot_token = os.environ.get('bot_token')
             #    
                 # set in debug
-            #    api_id = 7386053
-            #    api_hash = '78d1c032f3aa546ff5176d9ff0e7f341'
-            #    bot_token = '5124841893:AAH30p6ljtIzi2oPlaZwBmCfWQ1KelC6KUg'
+               api_id = 14681595
+               api_hash = 'a86730aab5c59953c424abb4396d32d5'
+              bot_token = '5561260685:AAFRMuPNvQpgoj7Mb2gW_WvMi_v-1MAGZU4'
 
             #    chat_id = int(update.message.chat.id)
             #    message_id = int(update.message.message_id)
@@ -470,7 +470,7 @@ def main():
     bot_token = os.environ.get('bot_token')
 
     #set in debug
-    bot_token = '5309657120:AAFxeVeoBUWXLr5
+    bot_token = '5561260685:AAFRMuPNvQpgoj7Mb2gW_WvMi_v-1MAGZU4'
 
     bot = ObigramClient(bot_token)
     bot.onMessage(onmessage)
